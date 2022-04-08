e1f853ac533aba857be3ea7252415d897985fff9
T-Urbo <urbanovich.tima@gmail.com>
2022-04-08 18:43:02 +0200

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Timothey Urbanovich on 08/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
